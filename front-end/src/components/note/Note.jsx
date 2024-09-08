c396f409979fcc76702d11dc1ee9837348f1bbd2
Amanda Chawinska <amanda.chawinska@spoko.pl>
2024-09-08 12:42:37 +0200

    note archiving - note restore button

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -1,7 +1,14 @@
 import RemoveIcon from "../../assets/remove.svg";
+import RestoreIcon from "../../assets/restore.svg";
 import styles from "./Note.module.css";
 import { TopBar } from "../top-bar/TopBar";
-import { useLoaderData, Form, useSubmit, redirect } from "react-router-dom";
+import {
+  useLoaderData,
+  Form,
+  useSubmit,
+  redirect,
+  useResolvedPath,
+} from "react-router-dom";
 import { useCallback } from "react";
 import { debounce } from "../../utils/debounce";
 
@@ -55,6 +62,7 @@ export async function updateNote({ request, params }) {
 const Note = () => {
   const note = useLoaderData();
   const submit = useSubmit();
+  const path = useResolvedPath();
 
   const onChangeCallback = useCallback(
     debounce((event) => {
@@ -67,6 +75,13 @@ const Note = () => {
   return (
     <div className={styles.container}>
       <TopBar>
+        {path.pathname.includes("archive") && (
+          <Form>
+            <button className={styles.button}>
+              <img className={styles.image} src={RestoreIcon} />
+            </button>
+          </Form>
+        )}
         <Form
           method="DELETE"
           action="delete"
@@ -83,7 +98,11 @@ const Note = () => {
             });
           }}
         >
-          <button className={styles.button}>
+          <button
+            className={`${styles.button} ${
+              path.pathname.includes("archive") ? styles["align-right"] : ""
+            }`}
+          >
             <img className={styles.image} src={RemoveIcon} />
           </button>
         </Form>
