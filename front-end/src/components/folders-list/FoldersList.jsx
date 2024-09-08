668cc37458da27a6f677cf2adf1fe70cfe639781
Amanda Chawinska <amanda.chawinska@spoko.pl>
2024-09-08 12:05:52 +0200

    archiving notes - adding a new path

--- front-end/src/components/folders-list/FoldersList.jsx
+++ front-end/src/components/folders-list/FoldersList.jsx
@@ -6,66 +6,67 @@ import { AddNewButton } from "../add-new-button/AddNewButton";
 import { NavLink, useLoaderData, Form, redirect } from "react-router-dom";
 
 export async function createFolder(args) {
-    const data = await args.request.formData();
-    const folderName = data.get("folder-name");
+  const data = await args.request.formData();
+  const folderName = data.get("folder-name");
 
-    return fetch(`http://localhost:3000/folders`, {
-        method: "POST",
-        body: JSON.stringify({
-            name: folderName,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        },
+  return fetch(`http://localhost:3000/folders`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: folderName,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then((response) => {
-            return response.json();
-        })
-        .then((newFolder) => {
-            return redirect(`/notes/${newFolder.id}`);
-        });
+    .then((newFolder) => {
+      return redirect(`/notes/${newFolder.id}`);
+    });
 }
 
 const Folders = ({ children }) => (
-    <div className={styles["folders-column"]}>{children}</div>
+  <div className={styles["folders-column"]}>{children}</div>
 );
 const UserCreatedFolders = ({ children }) => (
-    <div role="list" className={styles["folders-list"]}>
-        {children}
-    </div>
+  <div role="list" className={styles["folders-list"]}>
+    {children}
+  </div>
 );
 
 const FoldersList = () => {
-    const folders = useLoaderData();
+  const folders = useLoaderData();
 
-    return (
-        <Folders>
-            <TopBar>
-                <Form method="POST" action="/">
-                    <input
-                        className={styles["new-folder-input"]}
-                        type="text"
-                        placeholder="Nazwa folderu"
-                        name="folder-name"
-                    />
-                    <AddNewButton type="submit">+</AddNewButton>
-                </Form>
-            </TopBar>
+  return (
+    <Folders>
+      <TopBar>
+        <Form method="POST" action="/">
+          <input
+            className={styles["new-folder-input"]}
+            type="text"
+            placeholder="Nazwa folderu"
+            name="folder-name"
+          />
+          <AddNewButton type="submit">+</AddNewButton>
+        </Form>
+      </TopBar>
 
-            <Title>Foldery</Title>
-            <UserCreatedFolders>
-                {folders.map((folder) => (
-                    <NavLink key={folder.id} to={`/notes/${folder.id}`}>
-                        {({ isActive }) => {
-                            return (
-                                <Folder active={isActive}>{folder.name}</Folder>
-                            );
-                        }}
-                    </NavLink>
-                ))}
-            </UserCreatedFolders>
-        </Folders>
-    );
+      <Title>Foldery</Title>
+      <UserCreatedFolders>
+        {folders.map((folder) => (
+          <NavLink key={folder.id} to={`/notes/${folder.id}`}>
+            {({ isActive }) => {
+              return <Folder active={isActive}>{folder.name}</Folder>;
+            }}
+          </NavLink>
+        ))}
+      </UserCreatedFolders>
+      <NavLink to="archive">
+        <Folder>Archiwum </Folder>
+      </NavLink>
+    </Folders>
+  );
 };
 
 export default FoldersList;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -10,70 +10,77 @@ import { updateNote } from "./components/note/Note";
 import { NotFound } from "./components/not-found/NotFound";
 
 const router = createBrowserRouter([
-    {
-        element: <App />,
-        path: "/",
-        action: createFolder,
-        loader: () => {
-            return fetch("http://localhost:3000/folders");
-        },
-        shouldRevalidate: ({ formAction }) => {
-            if (formAction === "/") {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        errorElement: <NotFound />,
-        children: [
-            {
-                path: "notes/:folderId",
-                element: <NotesList />,
-                action: createNewNote,
-                loader: ({ params }) => {
-                    return fetch(
-                        `http://localhost:3000/notes?folderId=${params.folderId}`
-                    );
-                },
-                children: [
-                    {
-                        path: `note/:noteId`,
-                        element: <Note />,
-                        action: updateNote,
-                        errorElement: <NotFound />,
-                        loader: async ({ params }) => {
-                            const result = await fetch(
-                                `http://localhost:3000/notes/${params.noteId}`
-                            );
-
-                            if (result.status === 404) {
-                                throw new Error();
-                            }
-
-                            return result.json();
-                        },
-                        shouldRevalidate: ({ formAction }) => {
-                            if (formAction) {
-                                return false;
-                            } else {
-                                return true;
-                            }
-                        },
-                        children: [
-                            {
-                                path: "delete",
-                                action: deleteNote,
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
+  {
+    element: <App />,
+    path: "/",
+    action: createFolder,
+    loader: () => {
+      return fetch("http://localhost:3000/folders");
     },
+    shouldRevalidate: ({ formAction }) => {
+      if (formAction === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "archive",
+        element: <NotesList />,
+        loader: () => {
+          return fetch("http://localhost:3000/archive");
+        },
+      },
+      {
+        path: "notes/:folderId",
+        element: <NotesList />,
+        action: createNewNote,
+        loader: ({ params }) => {
+          return fetch(
+            `http://localhost:3000/notes?folderId=${params.folderId}`
+          );
+        },
+        children: [
+          {
+            path: `note/:noteId`,
+            element: <Note />,
+            action: updateNote,
+            errorElement: <NotFound />,
+            loader: async ({ params }) => {
+              const result = await fetch(
+                `http://localhost:3000/notes/${params.noteId}`
+              );
+
+              if (result.status === 404) {
+                throw new Error();
+              }
+
+              return result.json();
+            },
+            shouldRevalidate: ({ formAction }) => {
+              if (formAction) {
+                return false;
+              } else {
+                return true;
+              }
+            },
+            children: [
+              {
+                path: "delete",
+                action: deleteNote,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
